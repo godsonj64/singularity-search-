451afcbe903c8155a9a28f891c5defc1a78825ba
Godson Johnson <108623780+godsonj64@users.noreply.github.com>
2026-06-17 17:33:14 -0300

    Add classical package initializer

---
+++ singularity_search/classical/__init__.py
@@ -0,0 +1,5 @@
+"""Classical Origin-Coupled Spectral Diffusion Search."""
+
+from singularity_search.classical.ocsd import OCSDConfig, OriginCoupledSpectralDiffusionSearch
+
+__all__ = ["OCSDConfig", "OriginCoupledSpectralDiffusionSearch"]
